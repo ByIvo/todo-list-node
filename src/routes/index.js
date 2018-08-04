35446755aada8198f9240d17343e87687158dd49
Ivo Batistela <irbatistela@gmail.com>
2018-08-04 15:15:29 -0300

    added a hello world to test docker container and changed the exposed port

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ import tasksRoute from './tasks';
 const router = express.Router();
 
 router.use('/tasks', tasksRoute);
+router.use('/', (req, res) => {
+    res.send('Hello World');
+});
 
 export default router;
